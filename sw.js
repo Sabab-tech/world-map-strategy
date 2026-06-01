a8f0dac8e1346edeeab50ad349e346e607101781
Sabab-tech <fazlealahi1968@gmail.com>
2026-06-01 17:29:14 +0600

    Remove caching logic from service worker
    Remove service worker fetch event listener and cache logic.

--- sw.js
+++ sw.js
@@ -1,11 +1,1 @@
-const CACHE_NAME = 'map-cache-v1';
-self.addEventListener('fetch', (event) => {
-    event.respondWith(caches.match(event.request).then((response) => {
-        return response || fetch(event.request).then((res) => {
-            let clone = res.clone();
-            caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
-            return res;
-        });
-    }));
-});
-          
+
